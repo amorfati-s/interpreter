679f34f49dffbc325d311864e652e424ac71a3eb
amorfati-s <40743360+sonalishere@users.noreply.github.com>
2020-11-19 11:22:57 -0500

    Create interpret.c

---
+++ interpret.c
@@ -0,0 +1,136 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+short ax;
+short bx;
+short cx;
+short dx;
+
+#define LSIZ 128
+#define RSIZ 100
+
+int main(int argc, char *argv[])
+{
+  // declare shorts as registers
+  //read file in as datatype array of chars[] so we can refer to each line
+  char line[RSIZ][LSIZ];
+  int i = 1;
+  int tot = 0;
+  const char s[2] = " \t";
+  char *token;
+
+  FILE *fp = fopen(argv[1], "r");
+  while (fgets(line[i], LSIZ, fp))
+  {
+    line[i][strlen(line[i]) - 1] = '\0';
+    i++;
+  }
+
+  tot = i;
+  printf("\n The content in the file is : \n");
+  for (i = 0; i < tot; ++i)
+  {
+    printf(" %s\n", line[i]);
+
+    /////////////////////////////////////////////////////////
+    ///// I/O 
+    if (strstr(line[i], "read"))
+    {
+      printf(" hi \n");
+    }
+
+    if (strstr(line[i], "print"))
+    {
+      printf(" hi \n");
+    }
+
+    ///////////////////////////////////////////////////////
+    ////// MOVING DATA
+
+    if (strstr(line[i], "mov"))
+    {
+      printf(" hi \n");
+    }
+
+    //////////////////////////////////////////////////////////
+    ///// ARITHMETIC
+
+    if (strstr(line[i], "add"))
+    {
+      printf(" hi \n");
+    }
+    if (strstr(line[i], "mul"))
+    {
+      printf(" hi \n");
+    }
+    if (strstr(line[i], "sub"))
+    {
+      printf(" hi \n");
+    }
+    if (strstr(line[i], "div"))
+    {
+      printf(" hi \n");
+    }
+
+    ////////////////////////////////////////////////////
+    /////JMP STATEMENTS
+    if (strstr(line[i], "jmp"))
+    {
+      printf("\n hi \n");
+    }
+    if (strstr(line[i], "je"))
+    {
+      printf("\n hi \n");
+    }
+    if (strstr(line[i], "jne"))
+    {
+      printf("\n hi \n");
+    }
+    if (strstr(line[i], "jg"))
+    {
+      printf("\n hi \n");
+    }
+    if (strstr(line[i], "jge"))
+    {
+      printf("\n hi \n");
+    }
+    if (strstr(line[i], "jl"))
+    {
+      printf("\n hi \n");
+    }
+    if (strstr(line[i], "jle"))
+    {
+      printf("\n hi \n");
+    }
+
+  }
+  printf("\n");
+
+  fclose(fp);
+
+  //checking if lines printed
+  printf("\n Checking if the lines are stored into my line array : \n");
+  printf(" %s\n", line[1]);
+  printf(" %s\n", line[2]);
+  printf(" %s\n", line[3]);
+  printf(" %s\n", line[4]);
+
+  printf("\n Looking for token : \n");
+  //looking for token
+  token = strtok(line[1], s);
+
+  while (token != NULL)
+  {
+    printf(" %s", token);
+    token = strtok(NULL, s);
+  }
+
+  printf("\n");
+  printf("\n");
+  return 0;
+}
+
+void reg(char array[], short ax, short bx, short cx, short dx)
+{
+}
