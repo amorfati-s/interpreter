40e27a0effe030a689bc5db329ef7a0b51b1f6e2
amorfati-s <40743360+sonalishere@users.noreply.github.com>
2020-11-21 12:21:49 -0500

    Update interpret.c
    strtok_r

--- interpret.c
+++ interpret.c
@@ -15,27 +15,31 @@ int main(int argc, char *argv[])
   // declare shorts as registers
   //read file in as datatype array of chars[] so we can refer to each line
   char line[RSIZ][LSIZ];
-  int i = 1;
+  int i = 0;
   int tot = 0;
-  const char s[2] = " \t";
+  const char s[2] = " ";
   char *token;
-  char temp[100]; 
+  char temp[100];
+  char *saveptr;
 
   FILE *fp = fopen(argv[1], "r");
-  while (fgets(line[i], LSIZ, fp))
+  while (!feof(fp))
   {
-    line[i][strlen(line[i]) - 1] = '\0';
-    i++;
+    while (fgets(line[i], LSIZ, fp))
+    {
+      line[i][strlen(line[i]) - 1] = '\0';
+      i++;
+    }
   }
 
   tot = i;
-  printf("\n The content in the file is : \n");
+  printf("\n The content in the file is : \n\n");
   for (i = 0; i < tot; ++i)
   {
     printf("%s\n", line[i]);
 
     /////////////////////////////////////////////////////////
-    ///// I/O 
+    ///// I/O
     if (strstr(line[i], "read"))
     {
       printf(" hi \n");
@@ -104,7 +108,6 @@ int main(int argc, char *argv[])
     {
       printf("\n hi \n");
     }
-
   }
   printf("\n");
 
@@ -112,27 +115,31 @@ int main(int argc, char *argv[])
 
   //checking if lines printed
   printf("\n Checking if the lines are stored into my line array : \n");
+  printf(" %s\n", line[0]);
   printf(" %s\n", line[1]);
   printf(" %s\n", line[2]);
   printf(" %s\n", line[3]);
-  printf(" %s\n", line[4]);
 
   printf("\n Looking for token : \n");
   //looking for token
-  //strtok changes string input 
+  //strtok changes string input
 
   strcpy(temp, line[1]);
-  token = strtok(temp, s);
+  //printf("%s", temp);
+  //token = strtok(temp, s);
 
-  while (token != NULL)
-  {
+  token = strtok_r(temp, s, &saveptr); 
+    //while (token != NULL)
+    //{
     printf(" %s", token);
-    token = strtok(NULL, s);
-  }
+    //token = strtok(NULL, s);
+  
+  token = strtok
 
   printf("\n");
   printf("\n");
   return 0;
+
 }
 
 void reg(char array[], short ax, short bx, short cx, short dx)
